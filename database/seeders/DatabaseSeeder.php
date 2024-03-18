a4373a6a4bfdef44fdf15428207b0bdd1adafd4f
Malika Umaralieva <malika0umaralieva@gmail.com>
2024-03-18 16:39:48 +0100

    Database_beginningg

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RecipesSeeder::class,
             IndianRecipeSeeder::class,
-            IngredientsSeeder::class)
+            IngredientsSeeder::class
         ]);
     }
 }
