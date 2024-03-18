833ed32341870014b4213f89d3f4fa5ed59e89ab
akhiler <akhiler19@gmail.com>
2024-03-18 15:19:17 +0100

    created component indian cusine

---
+++ resources/js/recipes/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import IndianCusine from './IndianCusine'
+
+
+function App() {
+  return (
+   <IndianCusine/>
+   
+  )
+}
+
+export default App
--- resources/js/recipes/Main.jsx
+++ resources/js/recipes/Main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import App from './App'
 
 function Main() {
   return (
-    <div>Main</div>
+    <div>Main
+        <App/>
+    </div>
   )
 }
 
