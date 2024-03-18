41a902dbe90a70fdcb5dca66f5ff291f0708903e
Yusaiyin <yusaiyin89@gmail.com>
2024-03-18 15:16:51 +0100

    UyghurPage

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\RecipesController;
+use App\Models\Recipe;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/uyghur-cuisine',[RecipesController::class,'index'])->name('api.uyghur');
