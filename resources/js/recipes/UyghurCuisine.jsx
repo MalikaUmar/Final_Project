34837074ffc64411e9ff7a285e54576aa367ad11
Yusaiyin <yusaiyin89@gmail.com>
2024-03-19 08:59:22 +0100

    change style

--- resources/js/recipes/UyghurCuisine.jsx
+++ resources/js/recipes/UyghurCuisine.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./UyghurCuisine.scss";
+import { Link } from "react-router-dom";
 
 export default function UyghurCuisine() {
     const [uyRecipes, setUyRecipes] = useState([]);
