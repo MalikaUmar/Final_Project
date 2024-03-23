016490ca35888ee0aff210c64fcf7b83a4906865
akhiler <akhiler19@gmail.com>
2024-03-23 10:16:17 +0100

    merged

--- resources/js/recipes/App.jsx
+++ resources/js/recipes/App.jsx
@@ -12,7 +12,6 @@ import { useState } from "react";
 import { useEffect } from "react";
 import Favourites from "./Favourites";
 import Homepage from "./Homepage";
-import InsertNewRecipe from "./InsertNewRecipe";
 import SerbianCusine from "./SebianCusine";
 // import SearchFromIgredinents from "./SearchFromIgredinents";
 
@@ -30,17 +29,6 @@ function App() {
 
         if (response.status == 200) {
             const data = await response.json();
-            setUser(data);
-        }
-    };
-
-    const additemsToFavourites = async (recipe_id) => {
-        const response = await fetch(
-            `/api/addToFavourites/${recipe_id}/${user.id}`
-        );
-        const data = await response.json();
-        console.log(data);
-    };
             setUser(data)   
         } 
     }
@@ -59,23 +47,7 @@ const additemsToFavourites= async(recipe_id)=>{
 
     return (
         <>
-            <UserContext.Provider value={{user,getUser}}>
-            <FavouriteContext.Provider value={{additemsToFavourites,active,setActive,userActive,setUserActive}}>
 
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Homepage/>} />
-                    <Route path="/register" element={<Register/>} />
-                    <Route path="/login" element={<Login/>} />
-                    <Route path="/indian" element={<IndianCusine/>} />
-                    <Route path="/uyghur" element={<UyghurCuisine/>} />
-                    <Route path="/favourites" element={<Favourites/>} />
-                    <Route path="/search" element={<SearchFromIgredinents />} />
-                </Routes>   
-            </BrowserRouter>
-            </FavouriteContext.Provider>
-
-        </UserContext.Provider>  
             <UserContext.Provider value={{ user, getUser }}>
                 <FavouriteContext.Provider
                     value={{
