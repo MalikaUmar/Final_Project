f4ab07fd73e0ca851b490dcc27693791e55332c8
akhiler <akhiler19@gmail.com>
2024-03-25 13:19:44 +0100

    pulled

--- resources/js/recipes/App.jsx
+++ resources/js/recipes/App.jsx
@@ -1,15 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Homepage from "./Homepage";
-import Register from "./Register";
-import Login from "./Login";
+import React from "react";
 import IndianCusine from "./IndianCusine";
 import UyghurCuisine from "./UyghurCuisine";
-import SerbianCusine from "./SerbianCusine"; // Note: There's a typo in your component name - should it be "SerbianCuisine"?
-import Favourites from "./Favourites";
-import SearchFromIgredinents from "./SearchFromIngredients"; // Note: There's a typo - should it be "SearchFromIngredients"?
-import RecipeDetail from "./RecipeDetail";
 import ScrollToTop from "./ScrollToTop";
+import RecipeDetail from "./RecipeDetail";
+import Register from "./Register";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login.Jsx";
 import UserContext from "./UserContext";
 import FavouriteContext from "./FavouriteContext";
 import { useState } from "react";
@@ -18,11 +14,10 @@ import Favourites from "./Favourites";
 import Homepage from "./Homepage";
 import SerbianCusine from "./SebianCusine";
 // import SearchFromIgredinents from "./SearchFromIgredinents";
-import InsertNewRecipe from "./InsertNewRecipe";
 
 function App() {
     const [user, setUser] = useState(null);
-    const [active, setActive] = useState(false);
+    const [active, setActive] = useState(false); // state to display popwindow when adding recipes to favourites
     const [userActive, setUserActive] = useState(true);
 
     const getUser = async () => {
@@ -32,7 +27,7 @@ function App() {
             },
         });
 
-        if (response.status === 200) {
+        if (response.status == 200) {
             const data = await response.json();
             setUser(data)   
         } 
@@ -52,23 +47,7 @@ const additemsToFavourites= async(recipe_id)=>{
 
     return (
         <>
-            <UserContext.Provider value={{ user, getUser }}>
-                <FavouriteContext.Provider value={{ additemsToFavourites, active, setActive, userActive, setUserActive }}>
 
-                    <BrowserRouter>
-                        <Routes>
-                            <Route path="/" element={<Homepage />} />
-                            <Route path="/register" element={<Register />} />
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/indian" element={<IndianCusine />} />
-                            <Route path="/uyghur" element={<UyghurCuisine />} />
-                            <Route path="/favourites" element={<Favourites />} />
-                            <Route path="/search" element={<SearchFromIgredinents />} />
-                        </Routes>
-                    </BrowserRouter>
-                </FavouriteContext.Provider>
-
-            </UserContext.Provider>
             <UserContext.Provider value={{ user, getUser }}>
                 <FavouriteContext.Provider
                     value={{
@@ -87,19 +66,12 @@ const additemsToFavourites= async(recipe_id)=>{
                             <Route path="/register" element={<Register />} />
                             <Route path="/login" element={<Login />} />
                             <Route path="/indian" element={<IndianCusine />} />
-                            <Route
-                                path="/uyghur-cuisine"
-                                element={<UyghurCuisine />}
-                            />
-                            <Route
-                                path="/cuisine/:id"
-                                element={<RecipeDetail />}
-                            />
-                            <Route
-                                path="/favourites"
-                                element={<Favourites />}
-                            />
-                            <Route path="/add-recipe" element={<InsertNewRecipe />} />
+                            <Route  path="/uyghur-cuisine" element={<UyghurCuisine />} />
+                            <Route path="/cuisine/:id"  element={<RecipeDetail />} />
+                            <Route path="/favourites" element={<Favourites />}  />
+                            {/* <Route path="/search" element={<SearchFromIgredinents />}  /> */}
+                            <Route path="/serbian" element={<SerbianCusine />}  />
+                                 
                         </Routes>
                     </BrowserRouter>
                 </FavouriteContext.Provider>
