534cb6afa30679224c4fbaa73d5a9b113b32648f
Yusaiyin <yusaiyin89@gmail.com>
2024-03-28 14:07:25 +0100

    burger link modified

--- resources/js/recipes/BurgerIconWindow.jsx
+++ resources/js/recipes/BurgerIconWindow.jsx
@@ -1,33 +1,105 @@
-import React from 'react'
-import "./BurgerIconWindow.scss"
-import UserContext from './UserContext';
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./BurgerIconWindow.scss";
+import UserContext from "./UserContext";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 
-function BurgerIconWindow({logout,setBurgericon}) {
+function BurgerIconWindow({ logout, setBurgericon }) {
     const { user } = useContext(UserContext);
-  return (
-   
+    return (
         <div className="burgerIcon-container">
-             <div className="close-burger-icon" onClick={()=>{setBurgericon(false)}}> 
-             &times;
-             </div>
-            
-            <div className="burgerIcon-link" onClick={()=>{setBurgericon(false)}}><Link className="burgerIcon-link-link" to={"/"}>Home</Link></div>
-            <div className="burgerIcon-link" onClick={()=>{setBurgericon(false)}}>My Profile</div>
-            <div className="burgerIcon-link" onClick={()=>{setBurgericon(false)}}><Link className="burgerIcon-link-link" to={"/favourites"}>your Favourites</Link></div>
-            <div className="burgerIcon-link" onClick={()=>{setBurgericon(false)}}><Link className="burgerIcon-link-link" to={"/add-recipe"}>Add a Recipe</Link></div>
-            <div className="burgerIcon-link" onClick={()=>{setBurgericon(false)}}><Link className="burgerIcon-link-link" to={"/search"}>search By ingredients</Link></div>
-            <div className="burgerIcon-link" onClick={()=>{setBurgericon(false)}}>Help</div>
-            {
-                user ?
-                <div className="burgerIcon-link" onClick={(event)=>{logout(event);setBurgericon(false)}}>Logout</div>  
-                :
-                <div className="burgerIcon-link" onClick={()=>{setBurgericon(false)}}><Link className="burgerIcon-link-link" to={"/login"}>Login</Link></div>
-            }
-             
+            <div
+                className="close-burger-icon"
+                onClick={() => {
+                    setBurgericon(false);
+                }}
+            >
+                &times;
+            </div>
+            <Link className="burgerIcon-link-link" to={"/"}>
+                <div
+                    className="burgerIcon-link"
+                    onClick={() => {
+                        setBurgericon(false);
+                    }}
+                >
+                    Home
+                </div>
+            </Link>
+            <div
+                className="burgerIcon-link"
+                onClick={() => {
+                    setBurgericon(false);
+                }}
+            >
+                My Profile
+            </div>
+
+            <Link className="burgerIcon-link-link" to={"/favourites"}>
+                <div
+                    className="burgerIcon-link"
+                    onClick={() => {
+                        setBurgericon(false);
+                    }}
+                >
+                    your Favourites
+                </div>
+            </Link>
+
+            <Link className="burgerIcon-link-link" to={"/add-recipe"}>
+                <div
+                    className="burgerIcon-link"
+                    onClick={() => {
+                        setBurgericon(false);
+                    }}
+                >
+                    Add a Recipe
+                </div>
+            </Link>
+
+            <Link className="burgerIcon-link-link" to={"/search"}>
+                <div
+                    className="burgerIcon-link"
+                    onClick={() => {
+                        setBurgericon(false);
+                    }}
+                >
+                    search By ingredients
+                </div>
+            </Link>
+
+            <div
+                className="burgerIcon-link"
+                onClick={() => {
+                    setBurgericon(false);
+                }}
+            >
+                Help
+            </div>
+            {user ? (
+                <div
+                    className="burgerIcon-link"
+                    onClick={(event) => {
+                        logout(event);
+                        setBurgericon(false);
+                    }}
+                >
+                    Logout
+                </div>
+            ) : (
+                <Link className="burgerIcon-link-link" to={"/login"}>
+                    <div
+                        className="burgerIcon-link"
+                        onClick={() => {
+                            setBurgericon(false);
+                        }}
+                    >
+                        Login
+                    </div>
+                </Link>
+            )}
         </div>
-  )
+    );
 }
 
-export default BurgerIconWindow
+export default BurgerIconWindow;
